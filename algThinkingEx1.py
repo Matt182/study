c319cbc11b7bd253e4b09d63220e908ebd2f86fe
Matt182 <mattspring0@gmail.com>
2015-06-09 07:34:27 -0700

    Update algThinkingEx1.py

--- algThinkingEx1.py
+++ algThinkingEx1.py
@@ -5,6 +5,18 @@ EX_GRAPH0 = {0:set([1,2]),1:set([]),2:set([])}
 EX_GRAPH1 = {0:set([1,4,5]),1:set([2,6]),2:set([3]),3:set([0]),4:set([1]),5:set([2]),6:set([])}
 EX_GRAPH2 = {0:set([1,4,5]),1:set([2,6]),2:set([3,7]),3:set([7]),4:set([1]),5:set([2]),6:set([]),7:set([3]),8:set([1,2]),9:set([0,4,5,6,7,3])}
 
+def make_complete_graph(num_nodes):
+    """
+    all nodes connected with each other
+    """
+    result = {}
+    for node in range(num_nodes):
+        result[node] = set([])
+        for edge in range(num_nodes):
+            if edge != node:
+                result[node].add(edge)
+    return result
+
 def compute_in_degrees(digraph):
     result = {}
     res = []
