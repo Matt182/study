cd8d055f5c8c43a723fe11b56eea4299e217ec81
Matt182 <mattspring0@gmail.com>
2015-06-24 15:29:10 -0700

    Update algThinkingEx2.py

--- algThinkingEx2.py
+++ algThinkingEx2.py
@@ -1,37 +1,72 @@
+"""
+Connected components and graph resilience
+"""
 from collections import deque
 
 def bfs_visited(ugraph, start_node):
-    q = deque([]);
+    """
+
+    :param ugraph: undirected graph for example: {0:set([1,2]),1:set([]),2:set([])}
+    :param start_node: int
+    :return:travel thrue nodes
+    """
+    que = deque([]);
     visited = {start_node};
-    q.append(start_node)
-    while len(q) != 0:
-        j = q.popleft()
-        for h in ugraph[j]:
-            if h not in visited:
-                visited.add(h)
-                q.append(h)
+    que.append(start_node)
+    while len(que) != 0:
+        node = que.popleft()
+        for neighbour in ugraph[node]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                que.append(neighbour)
     return visited
 
 def cc_visited(ugraph):
+    """
+
+    :param ugraph: undirected graph for example: {0:set([1,2]),1:set([]),2:set([])}
+    :return:all connected components
+    """
     res = []
     keys = ugraph.keys()
     while len(keys) != 0:
         start = keys[0]
-        cc = bfs_visited(ugraph,start)
-        res.append(cc)
-        for i in cc:
-            if i in keys:
-                keys.remove(i)
+        ccom = bfs_visited(ugraph,start)
+        res.append(ccom)
+        for node in ccom:
+            if node in keys:
+                keys.remove(node)
     return res
 
 def largest_cc_size(ugraph):
+    """
+
+    :param ugraph:
+    :return:
+    """
     connected_components= cc_visited(ugraph)
     res = 0
-    for i in connected_components:
-        if len(i) > res:
-            res = len(i)
+    for concomp in connected_components:
+        if len(concomp) > res:
+            res = len(concomp)
     return res
 
 
-EX_GRAPH1 = {0:set([1,4,5]),1:set([2,6]),2:set([3]),3:set([0]),4:set([1]),5:set([2]),6:set([]),7:set([8]),8:set([])}
-print largest_cc_size(EX_GRAPH1)
+
+def compute_resilience(ugraph, attack_order):
+    """
+
+    :param ugraph:
+    :param attack_order:
+    :return:
+    """
+    res =[]
+    res.append(largest_cc_size(ugraph))
+    for node in attack_order:
+        ugraph.pop(node)
+        for ver in ugraph.keys():
+            if node in ugraph[ver]:
+                ugraph[ver].remove(node)
+        res.append(largest_cc_size(ugraph))
+    return res
+
